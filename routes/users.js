91007e89f6f62be9a54b99236ddf4f4a08ea9275
anzhihao <anzhihao@jd.com>
2019-09-18 11:43:11 +0800

    登录用户路由

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,19 @@
 const router = require('koa-router')()
-
+const { login } = require('../controller/users')
+const { SuccessModel, ErrorModel } = require('../model/resModel')
 router.prefix('/api/users')
 
 router.post('/login', async function (ctx, next) {
   const { username, password } = ctx.request.body
-  ctx.body = {
-    errorCode: 0,
-    username,
-    password
+  const loginData = await login(username, password)
+
+  if (loginData.username) {
+    ctx.session.username = data.username
+    ctx.session.realname = data.realname
+
+    ctx.body = new SuccessModel()
+    return
   }
+  ctx.body = new ErrorModel('登录失败')
 })
 module.exports = router
